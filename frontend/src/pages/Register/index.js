6a9d2f511eebe04239ab3c6c67d292c9911fb750
wfnovaes <wfnovaes@gmail.com>
2020-03-25 18:18:13 -0300

    Construção do layout

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function Register() {
+
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Hero"/>
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma ajude pessoas a encontrar os casos da sua ONG.</p>
+
+                    <Link to="/" className="back-link">
+                        <FiArrowLeft size="16" color="#E02041"/>
+                        Voltar para home
+                    </Link>
+                </section>
+                <form >
+                    <input placeholder="Nome da ONG"/>
+                    <input type="email" placeholder="E-mail"/>
+                    <input placeholder="WhatsApp"/>
+                    <div className="input-group">
+                        <input placeholder="Cidade"/>
+                        <input placeholder="UF" style={{width: 80}}/>
+                    </div>
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
+}
